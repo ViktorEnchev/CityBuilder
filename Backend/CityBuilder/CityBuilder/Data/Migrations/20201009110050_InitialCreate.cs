369bdd019ff6e87c95564a852323127655a568f8
Viktor Enchev <viktor.enchev.work@gmail.com>
2020-10-09 15:06:57 +0300

    Backend: Data layer implementation
        - Add City, Road, CityRoadNetwork entities
        - Add CityBuilderDbContext with EntityFramework
        - Add Migrations for database
        - Add docker-compose for postgres database

---
+++ Backend/CityBuilder/CityBuilder/Data/Migrations/20201009110050_InitialCreate.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace CityBuilder.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "City",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Road",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Road", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CityRoadNetwork",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CityId = table.Column<int>(nullable: false),
+                    RoadId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CityRoadNetwork", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CityRoadNetwork_City_CityId",
+                        column: x => x.CityId,
+                        principalTable: "City",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CityRoadNetwork_Road_RoadId",
+                        column: x => x.RoadId,
+                        principalTable: "Road",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CityRoadNetwork_CityId",
+                table: "CityRoadNetwork",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CityRoadNetwork_RoadId",
+                table: "CityRoadNetwork",
+                column: "RoadId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CityRoadNetwork");
+
+            migrationBuilder.DropTable(
+                name: "City");
+
+            migrationBuilder.DropTable(
+                name: "Road");
+        }
+    }
+}
